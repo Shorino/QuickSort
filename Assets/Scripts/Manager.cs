59c02a7c70eb47f9db0f530d99ac845124d6b473
Joel <simkalok0914@hotmail.com>
2020-11-28 16:54:07 +0800

    Initial push

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    float[] array = new float[8];
+
+    void Start()
+    {
+        InitializeArray(0, 16, 18, 2, 1, 3, 0, 20);
+        QuickSort.Sort(ref array, true);
+        Output();
+    }
+
+    void InitializeArray(float a, float b, float c, float d, float e, float f, float g, float h)
+    {
+        array[0] = a;
+        array[1] = b;
+        array[2] = c;
+        array[3] = d;
+        array[4] = e;
+        array[5] = f;
+        array[6] = g;
+        array[7] = h;
+    }
+
+    void Output()
+    {
+        print(array[0] + " , " + array[1] + " , " + array[2] + " , " + array[3] + " , " + array[4] + " , " + array[5] + " , " + array[6] + " , " + array[7]);
+    }
+}
---
+++ Assets/Scripts/QuickSort.cs
@@ -0,0 +1,71 @@
+﻿public class QuickSort
+{
+    public static void Sort(ref float[] array, bool accendingOrder)
+    {
+        PerformQuickSort(ref array, 0, array.Length);
+        if (!accendingOrder)
+        {
+            Reverse(ref array);
+        }
+    }
+    static void PerformQuickSort(ref float[] array, int low, int high)
+    {
+        if(low < high)
+        {
+            var j = Partition(ref array, low, high);
+            PerformQuickSort(ref array, low, j);
+            PerformQuickSort(ref array, j + 1, high);
+        }
+    }
+    static int Partition(ref float[] array, int low, int high)
+    {
+        var pivot = array[low];
+        var i = low;
+        var j = high;
+
+        while (i < j)
+        {
+            do
+            {
+                i++;
+                if(i >= array.Length)
+                {
+                    break;
+                }
+            } while (array[i] <= pivot);
+            do
+            {
+                j--;
+                if (j < 0)
+                {
+                    break;
+                }
+            } while (array[j] > pivot);
+
+            if (i < j)
+            {
+                Swap(ref array, i, j);
+            }
+        }
+        Swap(ref array, low, j);
+
+        return j;
+    }
+    static void Swap(ref float[] array, int index1, int index2)
+    {
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+    static void Reverse(ref float[] array)
+    {
+        float[] reversedArray = new float[array.Length];
+        int j = 0;
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            reversedArray[j] = array[i];
+            j++;
+        }
+        array = reversedArray;
+    }
+}
